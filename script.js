1821a1909ce682af705151d41a1824a0856739a8
José Antonio Rosales <jose.rosales.mald@gmail.com>
2023-10-03 10:33:20 -0600

    feat: search

--- script.js
+++ script.js
@@ -7,20 +7,32 @@ var people = [
   },
 ];
 
+var name, pwd, age;
+
 function show() {
   var list = document.getElementById('list');
   list.innerHTML = '';
   people.forEach((person, i) => {
     var li = document.createElement('li');
-    li.innerHTML = "Nombre: " + person.name + ", Contraseña:  " + person.pwd + ", Edad: " + person.age;
+    li.innerHTML =
+      'Nombre: ' +
+      person.name +
+      ', Contraseña:  ' +
+      person.pwd +
+      ', Edad: ' +
+      person.age;
     list.appendChild(li);
   });
 }
 
+function set() {
+  name = document.getElementById('name').value;
+  pwd = document.getElementById('pwd').value;
+  age = document.getElementById('age').value;
+}
+
 function insert_head() {
-  var name = document.getElementById('name').value;
-  var pwd = document.getElementById('pwd').value;
-  var age = document.getElementById('age').value;
+  set();
   people.unshift({
     name: name,
     pwd: pwd,
@@ -30,9 +42,7 @@ function insert_head() {
 }
 
 function insert_tail() {
-  var name = document.getElementById('name').value;
-  var pwd = document.getElementById('pwd').value;
-  var age = document.getElementById('age').value;
+  set();
   people.push({
     name: name,
     pwd: pwd,
@@ -42,8 +52,12 @@ function insert_tail() {
 }
 
 function remove_tail() {
-  alert("Quieres eliminar el último registro?");
-  people.pop();
+  if (people.length) {
+    alert('¿Quieres eliminar el último registro?');
+    people.pop();
+  } else {
+    alert('No hay personas restantes.');
+  }
   show();
 }
 
@@ -57,4 +71,20 @@ function sort_asc() {
 function sort_desc() {
   people.reverse();
   show();
+}
+
+function search() {
+  set();
+  let item = people.find((e) => e.name === name);
+  if (!item) {
+    alert('No existe la persona.');
+  } else {
+    document.getElementById('search').innerHTML =
+      'Nombre: ' +
+      item.name +
+      ', Contraseña:  ' +
+      item.pwd +
+      ', Edad: ' +
+      item.age;
+  }
 }
